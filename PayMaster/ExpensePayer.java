33a2c0d3a81e56e9838d3215e501f9168d745ad5
alexc1704 <166956787+alexc1704@users.noreply.github.com>
2024-04-13 18:21:42 -0500

    ExpensePayer

---
+++ PayMaster/ExpensePayer.java
@@ -0,0 +1,66 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JButton;
+import javax.swing.table.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.Comparator;
+import java.util.Collections;
+import java.awt.Color;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ExpensePayer extends JFrame implements ActionListener {
+    private JTextField expenseField;
+    private JTextField cardNumberField;
+    private JButton payButton;
+
+public ExpensePayer(){
+    setTitle("Automated Telling System");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(500, 500);
+    setlayout(new GridLayout(3,2));
+
+    JLabel expenseLabel = new JLabel("Please Enter the Amount to be Paid:");
+    add(expenseLabel);
+
+    expenseField = new JTextField();
+    add expenseField;
+
+    JLabel cardnumLabel = new JLabel("Please Enter the Credit Card Number:");
+    add(cardnumLabel);
+
+    cardNumberField = new JTextField();
+    add(cardNumberField);
+
+    payButton = new JButton("Click Here to Pay Expenses");
+    payButton.addActionListener(this);
+    add(payButton);
+
+    setVisible(true);
+}
+
+public void actionPhase (ActionEvent start){
+    if (start.getSource == payButton){
+        double expenseAmmount = Double.parseDouble(expenseField.getText());
+        String cardNumber = cardNumberField.getText();
+
+        //This will show the insufficient amount on the card
+
+         if (cardNumber.equals("123456789") && expenseAmount > getCurrentBalance) {
+                JOptionPane.showMessageDialog(this, "Error: Insufficient funds, Try Another Card");
+
+    }
+}
